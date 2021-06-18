403a249317243ac34c0be16563a8d8a244f97929
Wim Vandeweyer <wim@wimvandeweyer.be>
2021-06-18 15:51:07 +0200

    updated dismissable button styling

--- resources/views/livewire/flash-message.blade.php
+++ resources/views/livewire/flash-message.blade.php
@@ -7,7 +7,7 @@
             'icon' => 'info',
             'dismissable-bg-color' => 'bg-blue-50',
             'dismissable-bg-color-hover' => 'bg-blue-100',
-            'dismissable-icon-color' => 'bg-blue-500',
+            'dismissable-icon-color' => 'text-blue-500',
         ],
         'success' => [
             'bg-color'     => 'bg-green-50',
@@ -16,7 +16,7 @@
             'icon'         => 'check',
             'dismissable-bg-color' => 'bg-green-50',
             'dismissable-bg-color-hover' => 'bg-green-100',
-            'dismissable-icon-color' => 'bg-green-500',
+            'dismissable-icon-color' => 'text-green-500',
         ],
         'warning' => [
             'bg-color'     => 'bg-yellow-50',
@@ -25,7 +25,7 @@
             'icon'         => 'warning',
             'dismissable-bg-color' => 'bg-yellow-50',
             'dismissable-bg-color-hover' => 'bg-yellow-100',
-            'dismissable-icon-color' => 'bg-yellow-500',
+            'dismissable-icon-color' => 'text-yellow-500',
         ],
         'error' => [
             'bg-color'     => 'bg-red-50',
@@ -34,7 +34,7 @@
             'icon'         => 'error',
             'dismissable-bg-color' => 'bg-red-50',
             'dismissable-bg-color-hover' => 'bg-red-100',
-            'dismissable-icon-color' => 'bg-red-500',
+            'dismissable-icon-color' => 'text-red-500',
         ],
 ];
         
